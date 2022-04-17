b35287866fe264458845daf5132a567b1f7f738e
Matv1K <balahonovmatvej@gmail.com>
2022-04-17 16:44:11 +0300

    implemented shitty nested routing

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import CollectionItem from "../collection-item/collection-item.component";
 
 import "./collection-preview.styles.scss";
@@ -6,9 +7,9 @@ import "./collection-preview.styles.scss";
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview ">
+    <div className="preview">
       {items
-        .filter((item, ind) => ind < 4)
+        .filter((item, idx) => idx < 4)
         .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
